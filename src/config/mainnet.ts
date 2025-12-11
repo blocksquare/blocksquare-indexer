013243c74b1dcd42e935fff4fa97da0fdf75cdac
Simon0x <simon0x+git@proton.me>
2025-12-12 00:20:43 +0400

    fix: remove duplicate Landhive pool entry missing type property

--- src/config/mainnet.ts
+++ src/config/mainnet.ts
@@ -66,11 +66,6 @@ export const mainnetConfig: Config = {
       ),
       type: PropertyStakingPoolType.LANDHIVE,
     },
-    // Landhive Pool
-    {
-      address: getAddress('0xFC7cd245913691Fb2d305a0fc60A1DadD1eCdeBF'),
-      valuationAddress: getAddress('0x3C18b35E8E919224eA4099acC0d280Eda76A71C0'),
-    }
   ],
   propertyTokenOfferingAddress: getAddress(
     '0x25862c4fb4ce9d6ff9b463488e0ec656fa08de78'
