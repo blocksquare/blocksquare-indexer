77d46e12696cea6486e98e307e41ced7305a8d01
rauf322 <rauf.faizov.200271@gmail.com>
2026-01-15 12:40:39 +0100

    feat: added two new graphql schema tables and logic for 1inch order filled
    First graphql schema tables is ReferralCode that's where our Referrer is
    living and it has relationships to Referrals table.
    Second table is Referral which showing to us Referral address data of
    creation and what was first order

--- src/handler/OneInchPostInteraction.ts
+++ src/handler/OneInchPostInteraction.ts
@@ -79,5 +79,39 @@ OneInchPostInteraction.PostInteractionOrderFilled.handler(async ({ event, contex
     event.params.taker,
     'takerCount'
   );
+
+  const extraData = event.params.extraData;
+  const EMPTY_BYTES32 = '0x' + '0'.repeat(64);
+  //We are extracting and checking extraData if it's not null/undefined, if it's not empty bytes and it's length 66 which suppose to be 
+  if (!extraData || extraData.length !== 66 || extraData === EMPTY_BYTES32) {
+    return;
+  }
+  //After we get our referralCode from schema if it's not there, then it's invalid referralCode this referralCode isn't exist
+  const referralCode = await context.ReferralCode.get(extraData);
+  if (!referralCode) {
+    return;
+  }
+  //Extracting buyer wallet who is actually referral, and check if referralCode owner != buyer wallet
+  const buyerWallet = event.params.taker;
+  const buyerId = `${event.chainId}-${buyerWallet}`;
+  if (referralCode.wallet_id === buyerId) {
+    return;
+  }
+  //After we are checking if this Referral is already exist or not 
+  const referralId = `${event.chainId}-${referralCode.marketplace_id}-${buyerWallet}`;
+  const existingReferral = await context.Referral.get(referralId);
+  if (existingReferral) {
+    return;
+  }
+  //If everything above was false,then we can conclude that it's valid Referral and we create new Referral entity which embedded with referrer
+  const tradeId = `${event.chainId}-${propertyToken.contractAddress}-${event.transaction.hash}-${event.logIndex}`;
+  context.Referral.set({
+    id: referralId,
+    wallet_id: buyerId,
+    referrer_id: referralCode.id,
+    marketplace_id: referralCode.marketplace_id,
+    firstOrderTrade_id: tradeId,
+    createdAt: event.block.timestamp
+  });
 });
 
