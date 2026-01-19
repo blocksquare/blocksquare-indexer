81beb6444c94566b70a578cc839bd4e86b582c71
rauf322 <rauf.faizov.200271@gmail.com>
2026-01-19 13:02:04 +0100

    fix: remove ReferralCode schema we are addressing to User to validate referralCode

--- src/handler/OneInchPostInteraction.ts
+++ src/handler/OneInchPostInteraction.ts
@@ -23,15 +23,18 @@ OneInchPostInteraction.PostInteractionOrderFilled.handler(async ({ event, contex
     return;
   }
 
+  const tradeId = `${event.chainId}-${propertyToken.contractAddress}-${event.transaction.hash}-${event.logIndex}`;
+  const buyerWalletId = `${event.chainId}-${event.params.taker}`;
+
   context.PropertyTokenTrade.set({
-    id: `${event.chainId}-${propertyToken.contractAddress}-${event.transaction.hash}-${event.logIndex}`,
+    id: tradeId,
     chainID: event.chainId,
     transactionHash: event.transaction.hash,
     blockNumber: event.block.number,
     blockTimestamp: event.block.timestamp,
     propertyToken_id: propertyToken.id,
     maker_id: `${event.chainId}-${maker}`,
-    taker_id: `${event.chainId}-${event.params.taker}`,
+    taker_id: buyerWalletId,
     orderHash: event.params.orderHash,
     makerToken: makerToken,
     takerToken: takerToken,
@@ -82,34 +85,37 @@ OneInchPostInteraction.PostInteractionOrderFilled.handler(async ({ event, contex
 
   const extraData = event.params.extraData;
   const EMPTY_BYTES32 = '0x' + '0'.repeat(64);
-  //We are extracting and checking extraData if it's not null/undefined, if it's not empty bytes and it's length 66 which suppose to be 
   if (!extraData || extraData.length !== 66 || extraData === EMPTY_BYTES32) {
     return;
   }
-  //After we get our referralCode from schema if it's not there, then it's invalid referralCode this referralCode isn't exist
-  const referralCode = await context.ReferralCode.get(extraData);
-  if (!referralCode) {
+
+  const userBytes = extraData.slice(2);
+  const referrerUserId = `${event.chainId}-${userBytes}`;
+  
+  const referrerUser = await context.User.get(referrerUserId);
+  if (!referrerUser) {
     return;
   }
-  //Extracting buyer wallet who is actually referral, and check if referralCode owner != buyer wallet
-  const buyerWallet = event.params.taker;
-  const buyerId = `${event.chainId}-${buyerWallet}`;
-  if (referralCode.wallet_id === buyerId) {
+
+  const buyerWalletEntity = await context.Wallet.get(buyerWalletId);
+  
+  if (buyerWalletEntity?.user_id === referrerUserId) {
     return;
   }
-  //After we are checking if this Referral is already exist or not 
-  const referralId = `${event.chainId}-${referralCode.marketplace_id}-${buyerWallet}`;
+
+  const marketplaceId = propertyToken.certifiedPartner_id;
+  const referralId = `${event.chainId}-${marketplaceId}-${event.params.taker}`;
   const existingReferral = await context.Referral.get(referralId);
+  
   if (existingReferral) {
     return;
   }
-  //If everything above was false,then we can conclude that it's valid Referral and we create new Referral entity which embedded with referrer
-  const tradeId = `${event.chainId}-${propertyToken.contractAddress}-${event.transaction.hash}-${event.logIndex}`;
+
   context.Referral.set({
     id: referralId,
-    wallet_id: buyerId,
-    referrer_id: referralCode.id,
-    marketplace_id: referralCode.marketplace_id,
+    wallet_id: buyerWalletId,
+    referrer_id: referrerUserId,
+    marketplace_id: marketplaceId,
     firstOrderTrade_id: tradeId,
     createdAt: event.block.timestamp
   });
