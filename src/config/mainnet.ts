f04ef3cb1b3ffaf421b805b70871c2ba9828f7b8
Simon0x <simon0x+git@proton.me>
2026-03-27 05:25:24 +0400

    chore: remove oneInchPostInteraction from mainnet config

--- src/config/mainnet.ts
+++ src/config/mainnet.ts
@@ -84,5 +84,4 @@ export const mainnetConfig: Config = {
   ),
   zeroExAddress: getAddress('0xdef1c0ded9bec7f1a1670819833240f027b25eff'),
   zeroExStartBlock: 17337444,
-  oneInchPostInteractionAddress: getAddress('0xC8B2029bF486c62d2086D767bA1C23b9485da29E')
 };
